ef53cd2e1f564f68c9a820b2f2a46b2540e87667
Vinicius Henrique <viniciushenrique5419@hotmail.com>
2020-11-16 11:56:21 -0300

    Update ConexaoBancoDados.java

--- src/main/java/com/repairAutoCar/estrutura/persistencia/ConexaoBancoDados.java
+++ src/main/java/com/repairAutoCar/estrutura/persistencia/ConexaoBancoDados.java
@@ -1,6 +1,11 @@
 package com.repairAutoCar.estrutura.persistencia;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import com.repairAutoCar.estrutura.util.VariaveisProjeto;
 
 public class ConexaoBancoDados {
 	
@@ -23,9 +28,33 @@ public class ConexaoBancoDados {
 		return CONEXAO_BANCO_DADOS;
 	}
 	
+	public EntityManager getEntityManager() {
+		return FABRICA_CONEXAO.createEntityManager();
+	}
+	
 	private EntityManagerFactory getCriarFabricaConexao() {
-				
-		return null;
+		
+		Map<String, String> properties = new HashMap<String, String>();
+		
+		properties.put("javax.persistence.schema-generation.database.action", "drop-and-create");
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+		properties.put("hibernate.connection.url", "jdbc:mysql://localhost:3306/repairAutoCar?createDatabaseIfNotExist=true&useSSL=false");
+		properties.put("hibernate.connection.username", "root");
+		//properties.put("hibernate.connection.password", "root");
+		properties.put("hibernate.c3p0.min_size", "10");
+		properties.put("hibernate.c3p0.max_size" ,"20" );
+		properties.put("hibernate.c3p0.acquire_increment","1");
+		properties.put("hibernate.c3p0.idle_test_period" ,"3000");
+		properties.put("hibernate.c3p0.max_statements","50" );
+		properties.put("hibernate.c3p0.timeout","1800" );
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+		properties.put("useUnicode", "true");
+		properties.put("characterEncoding", "UTF-8");
+		properties.put("hibernate.default_schema", "projeto");
+		
+		return Persistence.createEntityManagerFactory(VariaveisProjeto.PERSISTENCE_UNIT_NAME, properties);
 	}
 	
 }
