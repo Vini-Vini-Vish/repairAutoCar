8ddbdc0a01596a85ab45c8e33065e4b2b51e5242
Vinicius Henrique <viniciushenrique5419@hotmail.com>
2020-11-16 11:56:50 -0300

    Update UsuarioService.java

--- src/main/java/com/repairAutoCar/model/service/UsuarioService.java
+++ src/main/java/com/repairAutoCar/model/service/UsuarioService.java
@@ -1,5 +1,40 @@
 package com.repairAutoCar.model.service;
 
-public class UsuarioService {
+import javax.persistence.EntityTransaction;
+import com.repairAutoCar.model.dao.UsuarioDao;
+import com.repairAutoCar.model.models.Usuario;
 
+public class UsuarioService extends ConexaoBancoService {
+	
+	private UsuarioDao usuarioDao;
+	
+	public UsuarioService() {
+		this.usuarioDao = new UsuarioDao(this.getEntityManager());
+	}
+
+	public Integer save(Usuario usuario) {
+		
+		EntityTransaction trx = this.getTransaction();
+		
+		try {
+			
+			trx.begin();
+			this.getUsuarioDao().save(usuario);
+			trx.commit();
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			if(trx.isActive()) {
+				trx.rollback();
+			}
+		} finally {
+			this.close();
+		}
+		
+		return 0;
+	}	
+
+	public UsuarioDao getUsuarioDao() {
+		return usuarioDao;
+	}
 }
