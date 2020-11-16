adcb0d9832ae774fac470936d5c97dcc6ac25638
Vinicius Henrique <viniciushenrique5419@hotmail.com>
2020-11-16 12:53:47 -0300

    Update UsuarioTest.java

--- src/test/java/repairAutoCar/UsuarioTest.java
+++ src/test/java/repairAutoCar/UsuarioTest.java
@@ -1,13 +1,14 @@
 package repairAutoCar;
 
 import static org.junit.Assert.assertTrue;
+import java.util.List;
 import org.junit.Test;
 import com.repairAutoCar.model.models.Usuario;
 import com.repairAutoCar.model.service.UsuarioService;
 
 public class UsuarioTest {
 
-	@Test
+	//@Test(expected = Exception.class)
 	public void salvarUsuarioBancoTest() {
 		
 		Usuario usuario = new Usuario();
@@ -25,5 +26,53 @@ public class UsuarioTest {
 
 		System.out.println("Gravando Registro do Usuario no Banco de dados");
 		assertTrue(true);
+	}
+	
+	//@Test(expected = Exception.class)
+	public void alterarUsuarioBancoDadosTeste() {
+			
+		Usuario usuario = new Usuario();
+				
+		usuario.setId(1);
+		
+		//usuario.setUsername("Lucas da Silva brito");
+		//usuario.setPassword("123456");
+		//usuario.setEmail("roberto@carlos.com.br");
+		//usuario.setAtivo(false);
+		//usuario.setAdmin(false);
+		
+		UsuarioService usuarioService = new UsuarioService();
+		
+		usuario = usuarioService.findById(usuario.getId());
+		
+		System.out.println(usuario.toString());
+		
+		usuario.setEmail("brito.s@alumno.ifsp.edu.br");
+		
+		usuarioService.save(usuario);
+		
+		System.out.println("Alteracao do usuario no banco de dados");
+		
+		//assertTrue(true);
+	}
+		
+	//@Test(expected = Exception.class)
+	public void listarTodosUsuarioTabelaUsuario() {
+		UsuarioService usuarioService = new UsuarioService();
+		List<Usuario> listaUsuario = usuarioService.findAll();
+		
+		for(Usuario usuario : listaUsuario) {
+			System.out.println(usuario.toString());
+		}
+	}
+		
+	//@Test
+	public void excluirUsuarioDaTabela() {
+		Usuario usuario = new Usuario();
+		usuario.setId(2);
+		
+		UsuarioService usuarioService = new UsuarioService();
+		
+		usuarioService.delete(usuario);
 	}
 }
