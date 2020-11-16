19d5efda98062fa5e1040feafebd8994c74d183f
Vinicius Henrique <viniciushenrique5419@hotmail.com>
2020-11-16 12:53:37 -0300

    Update GenericDao.java

--- src/main/java/com/repairAutoCar/model/dao/GenericDao.java
+++ src/main/java/com/repairAutoCar/model/dao/GenericDao.java
@@ -2,41 +2,59 @@ package com.repairAutoCar.model.dao;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
-import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class GenericDao<DADO, Id extends Serializable>{
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+public abstract class GenericDao<T, Id extends Serializable>{
 	
 	private EntityManager entityManager;	
 	
-	private final Class<DADO> classePersistencia;
+	private final Class<T> classePersistencia;
 	
 	@SuppressWarnings("unchecked")
 	public GenericDao(EntityManager entityManager) {
 	this.entityManager = entityManager;
-	this.classePersistencia = (Class<DADO>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+	this.classePersistencia = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
 	}
 	
-	public void save(DADO entity) {
+	public void save(T entity) {
 		this.getEntityManager().persist(entity);
 	}
 	
-	public void update(DADO entity) {
+	public void update(T entity) {
 		this.getEntityManager().merge(entity);
 	}
 	
-	public void remove(DADO entity) {
+	public void remove(T entity) {
 		this.getEntityManager().remove(entity);
 	}
 	
-	public DADO findById(Id id) {
+	public T findById(Id id) {
 		return this.getEntityManager().find(getClassePersistencia(), id);
 	}
+	
+	@SuppressWarnings("unchecked")
+	 public List<T> findAll(Class<T> classe){
+		 List<T> lista = new ArrayList<>();
+		 Query query = this.getEntityManager().createQuery("SELECT o FROM "+classe.getSimpleName()+" o");
+		 
+		 lista = query.getResultList();
+		 
+		 return lista;		 
+	 }
 	
 	public EntityManager getEntityManager() {
 		return entityManager;
 	}
+	
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
 
-	public Class<DADO> getClassePersistencia() {
+	public Class<T> getClassePersistencia() {
 		return classePersistencia;
 	}	
 }
